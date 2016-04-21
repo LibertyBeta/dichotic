fbf59862d4eb0c770eb6d8017888dee39dea79ab
Rainer Paskiewicz <rainer.paskiewicz@gmail.com>
2016-04-20 21:14:14 -0400

    Added ics

--- server/main.js
+++ server/main.js
@@ -4,9 +4,43 @@ import '../imports/api/weather.js';
 import '../imports/api/judges.js';
 
 //
-// WebApp.connectHandlers.use("/hello", function(req, res, next) {
-//
-//     const test = Dogs.find({}).fetch();
-//     res.writeHead(200, {'Content-Type': 'application/json'});
-//     res.end(JSON.stringify(test));
-// });
+WebApp.connectHandlers.use("/cal", function(req, res, next) {
+
+    const test = Dogs.find({}).fetch();
+    res.writeHead(200, {'Mime-Type': 'application/ics'});
+    let sample = [
+      'BEGIN:VCALENDAR',
+'VERSION:2.0',
+'CALSCALE:GREGORIAN',
+'BEGIN:VEVENT',
+'SUMMARY:Access-A-Ride Pickup',
+'DTSTART;TZID=America/New_York:20160802T103400',
+'DTEND;TZID=America/New_York:20160802T110400',
+'LOCATION:1000 Broadway Ave.\, Brooklyn',
+'DESCRIPTION: Access-A-Ride trip to 900 Jay St.\, Brooklyn',
+'STATUS:CONFIRMED',
+'SEQUENCE:3',
+'BEGIN:VALARM',
+'TRIGGER:-PT10M',
+'DESCRIPTION:Pickup Reminder',
+'ACTION:DISPLAY',
+'END:VALARM',
+'END:VEVENT',
+'BEGIN:VEVENT',
+'SUMMARY:Access-A-Ride Pickup',
+'DTSTART;TZID=America/New_York:20160802T200000',
+'DTEND;TZID=America/New_York:20160802T203000',
+'LOCATION:900 Jay St.\, Brooklyn',
+'DESCRIPTION: Access-A-Ride trip to 1000 Broadway Ave.\, Brooklyn',
+'STATUS:CONFIRMED',
+'SEQUENCE:3',
+'BEGIN:VALARM',
+'TRIGGER:-PT10M',
+'DESCRIPTION:Pickup Reminder',
+'ACTION:DISPLAY',
+'END:VALARM',
+'END:VEVENT',
+'END:VCALENDAR',
+    ];
+    res.end(sample.join("\r"));
+});
